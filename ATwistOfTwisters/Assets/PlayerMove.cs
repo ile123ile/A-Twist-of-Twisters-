039677442dd8bb18325a540c5b21370b46ac98da
Anamaria Ardila <anamariaardila@dyn-160-39-227-84.dyn.columbia.edu>
2018-10-12 15:25:15 -0400

    added character movement and camera control

--- ATwistOfTwisters/Assets/PlayerMove.cs
+++ ATwistOfTwisters/Assets/PlayerMove.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour {
 
+    public float speed;
+    public float jumpForce;
+    private Rigidbody rb;
+
 	// Use this for initialization
 	void Start () {
-		
+        rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void FixedUpdate () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+
+        if(Input.GetKeyDown("space")){
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private bool IsGrounded(){
+
+        return false;
+    }
 }
