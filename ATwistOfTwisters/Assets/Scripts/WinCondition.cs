12e57375a79486c7ba102aa7a31f986a6b4bb2cc
Peter Choi <jc4883@columbia.edu>
2018-10-19 15:21:04 -0400

    win condition

---
+++ ATwistOfTwisters/Assets/Scripts/WinCondition.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinCondition : MonoBehaviour {
+
+    //instance variable that we set to Unity, later used when collided
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("hello!", gameObject);
+        }
+    }
+}
