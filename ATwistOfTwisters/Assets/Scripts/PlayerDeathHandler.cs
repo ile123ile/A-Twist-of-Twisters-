e37481ef61fd9ed85a73a45614a4d841c87d4d62
ile123ile <bbravencofounder@gmail.com>
2018-11-02 15:18:05 -0400

    added death handler script

---
+++ ATwistOfTwisters/Assets/Scripts/PlayerDeathHandler.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerDeathHandler : DeathHandler {
+
+    override public void Die()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
