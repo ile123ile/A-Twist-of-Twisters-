cc509e3de74abfc4db1b607045c79c35ba898f40
ile123ile <bbravencofounder@gmail.com>
2018-12-07 15:07:07 -0500

    now player and orphans freeze on death

---
+++ ATwistOfTwisters/Assets/Scripts/FreezeOnDeath.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreezeOnDeath : DeathHandler {
+
+	private Rigidbody rb;
+
+	public void Start()
+	{
+		rb = gameObject.GetComponent<Rigidbody>();
+	}
+
+	override public void Die()
+    {
+    	rb.constraints = RigidbodyConstraints.FreezeAll;
+    }
+
+}
--- ATwistOfTwisters/Assets/Scripts/PlayerDeathHandler.cs
+++ ATwistOfTwisters/Assets/Scripts/PlayerDeathHandler.cs
@@ -6,13 +6,7 @@ using UnityEngine.SceneManagement;
 public class PlayerDeathHandler : DeathHandler {
 
     public float deathClock = 5;
-    private ArtManager art;
     private float timeOut = -1;
-
-    public void Start()
-    {
-        art = GetComponent<ArtManager>();
-    }
 
     override public void Die()
     {
